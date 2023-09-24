47241ab5d65b0b8c1ba9d3df4f6e749f50f48c75
acezor <145784180+acezor@users.noreply.github.com>
2023-09-24 16:52:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,24 +6,27 @@ int main()
 {
     int *b = new int[N_ELEMENTS]; 
     std::cout << "1-100 ertekek duplazasa" << std::endl; 
-
     for (int i = 0; i < N_ELEMENTS; i++) 
     {
         b[i] = i*2;
     }
 
     for (int i = 1; i < N_ELEMENTS; i++) 
+    {
         std::cout << "Ertek: " << b[i] << std::endl; 
     }
 
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag = 0; 
+    int atlag = 0;
     for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i]; 
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS; 
     std::cout << "Atlag: " << atlag << std::endl;
+
+      return 0;
+}
 
     return 0;
 }
