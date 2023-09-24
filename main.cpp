496f958b8d76fd560d3dc11a5bec1b7e7890e617
acezor <145784180+acezor@users.noreply.github.com>
2023-09-24 16:06:01 +0200

    hibák fellelése

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+constexpr int N_ELEMENTS = 100; // 101-re kell írni a 0-tól való kezdés miatt
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];  // hibás név
+    std::cout << '1-100 ertekek duplazasa' // nincs ;
+    for (int i = 1;) //hiányos a feltétel
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hibás a feltétel
     {
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; // nincs megadva érték
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
