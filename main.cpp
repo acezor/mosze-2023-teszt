cfc32cc820dec125bb7e290bda0a8975eb2c45a4
acezor <145784180+acezor@users.noreply.github.com>
2023-09-24 16:09:10 +0200

    kész feladat

--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100; // 101-re kell írni a 0-tól való kezdés miatt
+constexpr int N_ELEMENTS = 101;
 
 int main()
 {
-    int *b = new int[NELEMENTS];  // hibás név
-    std::cout << '1-100 ertekek duplazasa' // nincs ;
-    for (int i = 1;) //hiányos a feltétel
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa" << std::endl; 
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        b[i] = i * 2;
+        b[i] = i*2;
     }
-    for (int i = 0; i; i++) //hibás a feltétel
-    {
-        std::cout << "Ertek:"
-    }    
+
+    for (int i = 1; i < N_ELEMENTS; i++) 
+        std::cout << "Ertek: " << b[i] << std::endl; 
+    }
+
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // nincs megadva érték
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i]
+        atlag += b[i]; 
     }
-    atlag /= N_ELEMENTS;
+    atlag /= N_ELEMENTS; 
     std::cout << "Atlag: " << atlag << std::endl;
+
     return 0;
 }
